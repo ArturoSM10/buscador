8259fe123fe2c80d2c2e6639319d1d603174a9a1
Arturo Mendoza <jarsmdza@gmail.com>
2026-02-19 20:29:42 -0600

    Se agregaron algunos cambios al codigo js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,13 +1,3 @@
-const campos = {
-        marca: filtrarCampos,
-        year: filtrarCampos,
-        min: filtrarCampos,
-        max: filtrarCampos,
-        puertas: filtrarCampos,
-        transmision: filtrarCampos,
-        color: filtrarCampos
-    };
-
 const camposActualizados = {
     marca: '',
     year: '',
@@ -42,40 +32,43 @@ function agregarInfo(datos) {
     limpiarHtml();
     const resultados = document.querySelector('.resultados');
     agregarAlerta(datos);
-    datos.map( dato => {
+    datos.forEach((dato)=>{
         const {marca, modelo, year, precio, puertas, transmision, color} = dato;
         const resultado = document.createElement('P');
         resultado.classList.add('resultado');
         resultado.textContent = `${marca} ${modelo} - ${year} - ${puertas} puertas - transmisión: ${transmision} - precio: ${precio} - color: ${color}`;
         resultados.appendChild(resultado);
     });
+
 }
 
 function leerEventos() {
     const form = document.querySelector('.form');
-    form.addEventListener('change', e =>{
-        const id = e.target.id;
-        const valor = e.target.value
-        const validador = campos[id];
-        if (validador) {
-            camposActualizados[id] = valor;
-            validador(valor);
-        }
+    form.addEventListener('change', e => {
+        const {id, value} = e.target;
+        if(!(id in camposActualizados)) return;
+        camposActualizados[id] = value;
+        filtrarCampos()
     });
 }
 function filtrarCampos() {
-    const {marca, year, min, max, puertas, transmision, color} = camposActualizados;
+    let {marca, year, min, max, puertas, transmision, color} = camposActualizados;
+
+    year = (year !== '' ? Number(year) : year);
+    min = (min !== '' ? Number(min) : min);
+    max = (max !== '' ? Number(max) : max);
+    puertas = (puertas !== '' ? Number(puertas) : puertas);
 
     const nuevoArray = autos.filter(auto => {
-        return (marca === '' || marca === auto.marca.toLowerCase()) && 
-               (year === '' || Number(year) === auto.year) &&
-               (min === '' || auto.precio >= Number(min)) &&
-               (max === '' || auto.precio <= Number(max)) &&
-               (puertas === '' || auto.puertas === Number(puertas)) &&
+        return ((marca === '' || marca === auto.marca.toLowerCase()) && 
+               (year === '' || year === auto.year) &&
+               (min === '' || auto.precio >= min) &&
+               (max === '' || auto.precio <= max) &&
+               (puertas === '' || auto.puertas === puertas) &&
                (transmision === '' || transmision === auto.transmision.toLowerCase()) &&
-               (color === '' || color === auto.color.toLowerCase());
+               (color === '' || color === auto.color.toLowerCase()));
     });
-
+    agregarAlerta(nuevoArray);
     agregarInfo(nuevoArray);
 }
 
@@ -88,15 +81,18 @@ function limpiarHtml() {
 
 function agregarAlerta(entrada) {
     const alerta = document.querySelector('.resultados__alert');
+    alerta.classList.toggle('activo', entrada.length=== 0);
+    alerta.classList.toggle('inactivo', entrada.length!== 0);
 
-    if ((entrada.length === 0 && alerta.classList.contains('activo')) || (entrada.length !== 0 && alerta.classList.contains('inactivo'))) return;
 
-    else if(entrada.length === 0 && alerta.classList.contains('inactivo')) {
-        alerta.classList.remove('inactivo');
-        alerta.classList.add('activo');
-    }
-    else {
-        alerta.classList.remove('activo');
-        alerta.classList.add('inactivo');
-    }
+    // if ((entrada.length === 0 && alerta.classList.contains('activo')) || (entrada.length !== 0 && alerta.classList.contains('inactivo'))) return;
+
+    // else if(entrada.length === 0 && alerta.classList.contains('inactivo')) {
+    //     alerta.classList.remove('inactivo');
+    //     alerta.classList.add('activo');
+    // }
+    // else {
+    //     alerta.classList.remove('activo');
+    //     alerta.classList.add('inactivo');
+    // }
 }
